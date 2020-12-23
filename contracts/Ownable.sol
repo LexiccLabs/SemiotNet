ae0b0df6954fc14972e2855ee4d4edc34ef1976c
Gokul Alex <gokulsan@users.noreply.github.com>
2020-12-24 00:38:31 +0530

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.8.0;
+pragma experimental ABIEncoderV2;
 
 import "./Context.sol";
 /**
@@ -23,7 +24,7 @@ contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor () internal {
+    constructor () {
         address msgSender = _msgSender();
         _owner = msgSender;
         emit OwnershipTransferred(address(0), msgSender);
