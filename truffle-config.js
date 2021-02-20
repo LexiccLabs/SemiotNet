3bc7048bd98078d10b26bb9da67bbe4ba083168c
Gokul Alex <gokulsan@users.noreply.github.com>
2021-02-20 11:00:18 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -14,14 +14,14 @@ module.exports = {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
-      timeoutBlocks: 200,
+      timeoutBlocks: 1000,
       skipDryRun: true
     },
     bsc: {
       provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
       confirmations: 10,
-      timeoutBlocks: 200,
+      timeoutBlocks: 1000,
       skipDryRun: true
     },
   },
