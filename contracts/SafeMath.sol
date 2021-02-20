95dcf09f2218e772b29a81df98200aefbfe0129e
Gokul Alex <gokulsan@users.noreply.github.com>
2021-02-20 17:42:09 +0530

    Update SafeMath.sol

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache License 2.0
 
 pragma solidity >=0.6.0 <0.8.0;
+pragma experimental ABIEncoderV2;
 
 /**
  * @dev Wrappers over Solidity's arithmetic operations with added overflow
